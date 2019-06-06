10c79b85e99c30f25d5f3348a2b860607ab2f109
unknown <davidthecomposer@gmail.com>
2019-06-06 12:17:24 -0600

    edied

--- conf.js
+++ conf.js
@@ -3,7 +3,7 @@ exports.config = {
   //could be multiple frameworks here.
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['shop.js'],
-  //could connect to any file name.js, or multiple files as a suite
+  //could connect to any file name.js, or multiple files as a suite.
   multiCapabilities: [{
     browserName: 'firefox',
   }, {
